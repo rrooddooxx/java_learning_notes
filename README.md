034d1f57deceb09f66e8f4bf8073875e1a02ba03
Sebastián Kravetz <sebastiankravetz@icloud.com>
2024-04-08 18:26:16 -0400

    generics

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## Generics
 
-1. Nos ayudan reforar el checkeo de datos en tiempo de compilación.
+1. Nos ayudan reforzar el checkeo de datos en tiempo de compilación.
 
    Ejemplo: En una estructura de datos como _ArrayList_ nos permite asegurar qué tipo de datos
    se almacenarán en esta estructura.
@@ -15,19 +15,29 @@
     // 👎
    
     List aGenericList = new ArrayList();
-
-    aGenericList.add(1);
     aGenericList.add("1"); // crashea en runtime
+    aGenericList.add(1);
 
-    System.out.println(aGenericList);
-   
    // 👍
    
    List<Integer> aNumbersList = new ArrayList<>();
     aNumbersList.add(1);
-   
-    aNumbersList.add("1"); // el compilador no permitirá esto
-   
-    
+    aNumbersList.add("1"); // el compilador no permitirá esto, seremos notificados por el IDE antes de ejecutar el programa.
+    ```
 
-```
+2. Para generalizar el tipado de una estructura, existen:
+    1. Type parameters: Son los _placeholders_ que indican en donde debe especificarse un tipo
+       concreto de datos, ejemplo:
+   ```java
+      public interface Map<K, V> 
+   // en donde K= primer type parameter
+   // y V= segundo type parameter
+   ``` 
+    2. Type arguments: Es cuando, en la posición de los _placeholders_ de los type parameters,
+       declaramos el tipo de dato concreto al inicializar una estructura de datos.
+   ```java
+      Map<String, Integer> myMap = new HashMap<>();
+   // en la posición K declaramos el tipo String
+   // en la posición V declaramos el tipo Integer
+   ``` 
+      
--- generics/src/Main.java
+++ generics/src/Main.java
@@ -1,20 +1,46 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
+        // EJ1.
+
+        // MALO
+        List aGenericList = new ArrayList();
+
+        aGenericList.add(1);
+        aGenericList.add("1");
+
+        System.out.println(aGenericList);
+
+    /*for (Object element : aGenericList) {
+      Integer el = (Integer) element;
+       -- this produces a runtime crash "class java.lang.String cannot be cast to class java.lang.Integer"
+      System.out.println(el);
+    }*/
+
+        // BUENO
+        List<Integer> aNumbersList = new ArrayList<>();
+        aNumbersList.add(1);
+        // aNumbersList.add("1"); -- forbidden by compiler, thnxs to generics :)
+        // now executing methods for Integers is safe
+
+        // EJ2.
+        Comparable<Integer> number = 10; // provides safety for operations with implicit casting
+        System.out.println(number.compareTo(11));
+
+        // EJ3.
+        List<String> listaNombres = new ArrayList<>();
+
+        Map<String, Boolean> mapOfFlags = new HashMap<>();
+        mapOfFlags.put("isReady", false);
+        mapOfFlags.put("wasExecuted", false);
+        System.out.println(mapOfFlags);
 
 
-    // MALO
-    List aGenericList = new ArrayList();
-
-    aGenericList.add(1);
-    aGenericList.add("1");
-
-    System.out.println(aGenericList);
-
-    // BUENO
-
-    List<Integer> aNumbersList = new ArrayList<>()
-  }
+    }
 }
